d46c4425167a9f776f77ed2e8f11b67584dde50f
jafow <jared.a.fowler@gmail.com>
2017-11-22 20:09:25 -0800

    Lint & fix syntax

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ var RtmClient = require('@slack/client').RtmClient
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS
 var keys = require('./key.json').SLACK_BOT_TOKEN
-var bot_token = keys.SLACK_BOT_TOKEN
+var token = keys.SLACK_BOT_TOKEN
 var CATEGORYLIST = require('./category-list.json')
 var groupName = keys.GROUP_NAME
-var rtm = new RtmClient(bot_token)
+var rtm = new RtmClient(token)
 
 var channel
 var channelMembers = []
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   for (let c of rtmStartData.groups) {
-    if (c.name = groupName) {
+    if (c.name.toLowerCase() === groupName.toLowerCase()) {
       channel = c.id
       channelMembers = c.members.map(m => m.toLowerCase())
     }
   }
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name} on channel ${channel}`);
-});
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name} on channel ${channel}`)
+})
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
   // rtm.sendMessage('HEYYY', channel)
 })
 
-rtm.on(RTM_EVENTS.MESSAGE, function onMessage(msg) {
+rtm.on(RTM_EVENTS.MESSAGE, function onMessage (msg) {
   if (isToPunbot(msg.text)) {
     let msgArray = msg.text.split(' ').map(xs => xs.toLowerCase())
     // check commands
@@ -33,7 +33,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function onMessage(msg) {
       let whitePun = getRandomCategory(CATEGORYLIST.white)
       let greenPun = getRandomCategory(CATEGORYLIST.green)
       rtm.sendMessage(':zap: *Punderdome on Slack* :zap: \n\n ' + whitePun + '::' + greenPun, msg.channel)
-    } else if (msgArray[1] == '++' ) {
+    } else if (msgArray[1] === '++') {
       // points for someone... check they are in the group
       if (channelMembers.indexOf(stripBrackets(msgArray[2])) < 0) {
         // they aren't in the group
@@ -45,13 +45,13 @@ rtm.on(RTM_EVENTS.MESSAGE, function onMessage(msg) {
   }
 })
 
-rtm.start();
+rtm.start()
 
 function isToPunbot (msgText) {
-  return /U85AZNDSS/.test(msgText);
+  return /U85AZNDSS/.test(msgText)
 }
 
-function getRandomCategory(list) {
+function getRandomCategory (list) {
   var len = list.length
   var randIdx = Math.floor(Math.random() * len)
   return list[randIdx]
